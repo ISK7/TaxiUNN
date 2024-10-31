390a2c43e7b9490417bac73a1c214b36c20619af
ISK7 <105171814+ISK7@users.noreply.github.com>
2024-10-31 22:24:02 +0300

    Добавил link в Server

--- Registrationtemplate/app/src/main/java/com/example/registrationtemplate/generalData/Server.java
+++ Registrationtemplate/app/src/main/java/com/example/registrationtemplate/generalData/Server.java
@@ -5,6 +5,8 @@ import java.net.URL;
 //Класс для работы с сервером
 public class Server {
 
+    private final static String link = "https://virtserver.swaggerhub.com/vinicuktimofei/taxiUNN/1.0.0";
+
     private final static int port = 8080;
 
     public static int getPort() {
